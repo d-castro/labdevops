de4b275999e7649822518e8b5dadebbcf0340280
Diego <diego.crisostomo@hotmail.com>
2022-05-10 23:00:37 -0300

    Desafio: Custom Message

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def pagina_inicial():
-    return "Hello World"
+    return "Aula top Gaby, by Diego Crisostomo"
 
 if __name__ == '__main__':
     app.run()
